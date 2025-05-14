6a889ef378bb71ef5e69c1b4a5464efa590f4b46
danielo2318 <aroctaviodaniel9@gmail.com>
2025-05-13 23:04:39 -0600

    I used a different method to get the dates

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,6 +1,24 @@
-document.getElementById("currentyear").textContent = new Date().getFullYear();
+const year = document.querySelector("#year");
+const today = new Date();
+
+year.innerHTML = `${today.getFullYear()}`;
+
+let lastModified = document.querySelector("#lastModified");
+lastModified.textContent = `Last modified ${document.lastModified}`;
+
+//2° manera utilizando getElementById
+/* document.getElementById("currentyear").textContent = new Date().getFullYear();
 
 // Obtiene la fecha de última modificación y la inserta en el párrafo con id "lastModified"
 document.getElementById(
   "lastModified"
-).textContent = `Última modificación: ${document.lastModified}`;
+).textContent = `Last modified: ${document.lastModified}`; */
+
+//3° manera usando toLocaleString()
+/* let lastModified = document.querySelector("#lastModified");
+let lastmodification = Date.parse(document.lastModified);
+
+// Convertimos el número a una fecha legible
+let formattedDate = new Date(lastmodification).toLocaleString();
+
+lastModified.textContent = `Last modified: ${formattedDate}`; */
